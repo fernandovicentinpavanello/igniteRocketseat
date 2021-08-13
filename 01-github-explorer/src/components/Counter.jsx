5b64633283be21cedfce3b598603cbe0587333c1
Luiz Pavanello <luizpavanello@gmail.com>
2021-08-13 15:40:35 -0300

    Key Concepts

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -10,7 +10,9 @@ export function Counter() {
   return (
     <div>
       <h2>{counter}</h2>
-      <button type="button" onClick={increment}>Increment</button>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
     </div>
   );
 }
